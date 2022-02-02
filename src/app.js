30b245b58a4f8dc3fd8f0848221f5001bdf685fa
Felipe <manolofelipec74@gmail.com>
2022-02-02 16:27:38 -0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.disable('x-powered-by');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.get('/', function(req, res) {
+    return res.status(200).send('Hola Mundo!!!');
+});
+
+app.listen(3000, () => console.log('Server listening on port 3000'));
